1984781118bc2c6f8a4d8fa7e076c8553c009f5d
Pasero Christian <c.pasero@student.tugraz.at>
2023-07-15 18:40:26 +0200

    fixed bug empty settings

--- src/db/local_db.js
+++ src/db/local_db.js
@@ -7,20 +7,16 @@ db.version(1).stores({
     settings: 'id, theme'
 });
 
-init()
-
-export async function init() {
-    const settings_exist = (await db.settings.toArray()).length
-    if (!settings_exist) {
-        db.settings.add({'id': 0, 'theme': 'theme-yellow'})
-    }
-}
 
 export async function setThemeLocalDb(theme) {
     db.settings.update(0, {'theme': theme})
 }
 
 export async function getTheme() {
-    const curr_theme = (await db.settings.toArray())[0].theme
-    return curr_theme;
+    const curr_theme = await db.settings.toArray()
+
+    if (!curr_theme.length) {
+        db.settings.add({'id': 0, 'theme': 'theme-yellow'})
+    }
+    return curr_theme[0].theme;
 }
