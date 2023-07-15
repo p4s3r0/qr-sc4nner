22d56cacab231cff8a401f7a946723c649c655d2
Pasero Christian <c.pasero@student.tugraz.at>
2023-07-15 18:18:58 +0200

    changed title bar of pwa

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    name: 'QRsc4n',
+    themeColor: '#151515',
+    msTileColor: '#151515',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: '#151515',
+    workboxOptions: {
+      skipWaiting: true
+    }
+  },
 })
